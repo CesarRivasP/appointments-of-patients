f951310081a41a92611bf9c8baf51033a060e2c5
CesarRivasP <cesarjose58@gmail.com>
2019-05-22 15:26:52 -0400

    Added divider component

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
+
 
 const Header = (props) => {
   const { title } = props;
   return (
-    <header>
-      <h1 className="text-center">{title}</h1>
-    </header>
+    <div>
+      <header>
+        <h1 className="text-center">{title}</h1>
+      </header>
+      
+      <Divider style={{ backgroundColor: 'white'}}/>
+    </div>
   );
 }
 
