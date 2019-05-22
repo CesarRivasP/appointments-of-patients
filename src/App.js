d153ffa6eae5a81ea5cd9f980a1afd67531d0402
CesarRivasP <cesarjose58@gmail.com>
2019-05-22 14:34:11 -0400

    Implemented header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/header';
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="container">
+      <Header title="Administrador de Pacientes de Veterinario"/>
     </div>
   );
 }
