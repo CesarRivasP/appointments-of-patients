f8b161706ba0a14b2f9a5d27c840ef7a94b8f0b4
CesarRivasP <cesarjose58@gmail.com>
2019-05-22 15:07:55 -0400

    Implemented component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/header';
+import AddAppointment from './components/add-appointment';
 
 
 function App() {
   return (
     <div className="container">
       <Header title="Administrador de Pacientes de Veterinario"/>
+      <div className="row">
+        <div className="col-md-6">
+          <AddAppointment />
+        </div>
+      </div>
     </div>
   );
 }
