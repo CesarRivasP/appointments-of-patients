415948ee7950a3b1346975dd4524ca0c2c239d41
CesarRivasP <cesarjose58@gmail.com>
2019-05-22 18:39:37 -0400

    Implemented info of appointments

--- src/components/appointment.js
+++ src/components/appointment.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Appointment = (props) => {
+  const { petName, ownerName, appointmentDate, appointmentTime, symptom } = props.info;
   return (
     <div className="media mt-3">
       <div className="media-body">
-        <h3 className="mt-0">Nombre de mascota</h3>
-        <p className="card-text"><span>Nombre del Dueño: </span></p>
-        <p className="card-text"><span>Fecha: </span></p>
-        <p className="card-text"><span>Hora: </span></p>
-        <p className="card-text"><span>Sintomas: </span></p>
+        <h3 className="mt-0"><span>{ petName }</span></h3>
+        <p className="card-text"><span>Nombre del Dueño: </span>{ ownerName }</p>
+        <p className="card-text"><span>Fecha: </span>{ appointmentDate }</p>
+        <p className="card-text"><span>Hora: </span>{ appointmentTime }</p>
+        <p className="card-text"><span>Sintomas: </span>{ symptom }</p>
         <p className="card-text"> </p>
       </div>
     </div>
