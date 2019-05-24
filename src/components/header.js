110dd3e538bd039a5c315418f9b1a0654608c6d7
CesarRivasP <cesarjose58@gmail.com>
2019-05-23 21:20:01 -0400

    Implemented proptypes for header componenet

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Divider from '@material-ui/core/Divider';
 
 
@@ -9,10 +10,14 @@ const Header = (props) => {
       <header>
         <h1 className="text-center">{title}</h1>
       </header>
-      
+
       <Divider style={{ backgroundColor: 'white'}}/>
     </div>
   );
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
 }
 
 export default Header;
